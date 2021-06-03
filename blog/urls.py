4d4487824991d798a0ad02e893d671d23c41761f
amanbai1608 <amanbai1608@gmail.com>
2021-06-03 17:38:26 +0600

    2 commit

--- blog/urls.py
+++ blog/urls.py
@@ -18,8 +18,12 @@ from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import path
 
+from main import views
+
+
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', views.index_page, name='index-page')
 ]
 if settings.DEBUG:
     urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
--- main/views.py
+++ main/views.py
@@ -1,3 +1,26 @@
+from django.http import HttpResponse
 from django.shortcuts import render
 
-# Create your views here.
+from .models import Category
+
+
+def index_page(request):
+    categories = Category.objects.all()
+    return render(request, 'main/index.html', {'categories': categories})
+# TODO: Переписать при помощи классов
+
+
+
+
+
+
+# TODO: Список постов
+# TODO: Переход по страницам
+# TODO: Регистрауия, активация, логин, логаут
+# TODO: Фильтрация, поиск сортировка
+# TODO: Пагинация
+# TODO: Переиспользование шаблонов
+# TODO: Проверка прав
+# TODO: Избранное
+# TODO: Дизайн
+# TODO: Описание (README)
